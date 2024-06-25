41059d71724ef00c319d57d3b661b94f5d050690
Kenneth A Wagner <kenw2190@gmail.com>
2024-06-24 19:56:29 -0500

    added API key and event listener to test API calls.

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,35 +1,37 @@
-const cityInput = document.getElementById("city-input");
-const submitBtn = document.getElementById("submit-button");
-const cityFormEl = document.getElementById("city-form");
-const APIKey; 
-const queryURL; 
-let city; 
+const cityInput = $('#city-input');
+const submitBtn = $('#submit-button');
+const cityFormEl = $("#city-form");
+const APIKey = `89c2d10cea5bf468636c45b15924d79d`;
+const queryURL = ``;
+let city;
 
 const formSubmitHandler = function (event) {
     event.preventDefault();
-     city = cityInput.value.trim();
+    city = cityInput.val();
     console.log(city);
 
 
-    let queryURL= `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${APIKey}`;
+    let queryURL = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${APIKey}`;
 
-fetch(queryURL)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data);
-        console.log("lat " + data[0].lat);
-        console.log("lon " + data[0].lon);
-    
-    });
+    fetch(queryURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            console.log("lat " + data[0].lat);
+            console.log("lon " + data[0].lon);
+
+        });
 };
 
-function getAPIs () {
-    // space for weather API fetch request
+submitBtn.on('click', formSubmitHandler);
 
-    // space for Marvel API fetch request
+function getAPIs() {
+    // Location API URL: 
 
-    // testing another push
+    // Weather API URL: 
+
+    // Marvel API URL: 
 
 }
