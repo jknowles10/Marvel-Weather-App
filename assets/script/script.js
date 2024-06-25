4fb9906171ccad83bc72e00346cf410af9eb1316
Kenneth A Wagner <kenw2190@gmail.com>
2024-06-25 18:45:11 -0500

    testing merge

--- assets/script/script.js
+++ assets/script/script.js
@@ -3,7 +3,7 @@ const submitBtn = $('#submit-button');
 const cityFormEl = $("#city-form");
 const APIKey = `89c2d10cea5bf468636c45b15924d79d`;
 let city;
-
+// testing
 // functions to store and retrieve the lat and lon from the location API in local storage so we can pull it out in the weather API.
 function storeLocation(lat, lon) {
     localStorage.setItem('lat', JSON.stringify(lat));
